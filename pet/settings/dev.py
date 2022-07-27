419607c4de5cbff38df221ed367ebb12292347ca
midhunibdc <midhunibdc@github.com>
2022-07-27 12:08:33 +0530

    initial commit

---
+++ pet/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-cf@p9r1g4(7f7*l=x@@4f5q-@!$*auxnsi$k=$wqo3bq-&ls)g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
